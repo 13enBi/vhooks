622b94a26c1f0f884d9358bc5fccadd7b09e57c1
13enBi <2210576534@qq.com>
2020-11-11 22:38:58 +0800

    useUnMounted

--- packages/useMounted.ts
+++ packages/useMounted.ts
@@ -1,9 +1,4 @@
-import { getCurrentInstance, onMounted, ComponentInternalInstance } from 'vue';
+import { getCurrentInstance, onMounted } from 'vue';
 
-const useMounted = (hook: () => any, target: ComponentInternalInstance | null = getCurrentInstance()) => {
-	const isMounted = target?.isMounted;
-
-	return isMounted ? hook() : onMounted(hook, target);
-};
-
-export default useMounted;
+export default (hook: () => any, target = getCurrentInstance()) =>
+	target?.isMounted ? hook() : onMounted(hook, target);
